6804133e028e6eab8c2d8fdc1c28a6d72476b83c
unknown <astulzer@hotmail.com>
2020-10-04 21:15:22 -0300

    biblioteca commit

---
+++ src/bibliotecas/TestaEquals.java
@@ -0,0 +1,28 @@
+package bibliotecas;
+
+public class TestaEquals {
+	public static void main(String[] args) {
+		
+	
+	int i = 0;
+	int j = 0;
+	
+	if(i==j) {
+		System.out.println("i e j são iguais");
+	} else {
+		System.out.println("i e j são diferentes");
+	}
+	String c = new String("Senac 456");
+	String d = new String("Senac 456");
+	if(c==d) {
+		System.out.println("c e d são iguais");
+	} else {
+		System.out.println("c e d são diferentes");
+	}
+	if(c.equals(d)) {
+		System.out.println("As Strings c e d possuem o mesmo conteudo");
+	}else {
+		System.out.println("As Strings c e d possuem conteudos diferentes");
+	}
+}
+}
